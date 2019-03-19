2ea9e2967eff86371621e55fe863acdc4a86067a
dzakaeryan20 <dzakaeryan20@gmail.com>
2019-03-19 11:37:04 +0700

    commit

---
+++ config/setting.js
@@ -0,0 +1,8 @@
+module.exports ={
+    "key":"271495250190737",
+    "secret":"4404e651526c9eb2d8418a97078043f0",
+    "callback":"http://localhost:3000/auth/facebook/callback",
+    'profileFields' : ['id', 'email', 'name'],
+    'token':'EAAD27GnyIZAEBADIZBiUnnrTtEowwfZA2SlOm72cxuXf6BJikylhsLbrk1ZCalAEZCw23WURln4ThZAX3gSqZB2ahJhUtj1xoen5eCMAcTKiCr7G2A04NQQ3d252SRoaSykOT2oZAW3rwZAuL8V7N1SWA0KyDdascwU7ereomu9pmfIS9vmZCAvYZCxRrkhxetHJ4eXAqwzvHGOvQZDZD',
+
+}
